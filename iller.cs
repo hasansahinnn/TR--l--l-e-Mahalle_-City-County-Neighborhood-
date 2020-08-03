5c71f73eecd806047fbb9740994bd3cd7f5c89f4
inosoma <42390330+inosoma@users.noreply.github.com>
2020-08-03 15:53:36 +0300

    Update iller.cs

--- iller.cs
+++ iller.cs
@@ -9,22 +9,26 @@ namespace ConsoleApp6
    
     public class il
     {
+        public int Id { get; set; }
         public string Il { get; set; }
         public List<ilce> Ilce { get; set; }
     }
     public class ilce
     {
+        public int Id { get; set; }
         public string Ilce { get; set; }
         public List<semt> Semt { get; set; }
     }
     public class semt
     {
+        public int Id { get; set; }
         public string Semt { get; set; }
         public string PostaKodu { get; set; }
         public List<mahalle> Mahalle { get; set; }
     }
     public class mahalle
     {
+        public int Id { get; set; }
         public string Mahalle { get; set; }
     }
 
